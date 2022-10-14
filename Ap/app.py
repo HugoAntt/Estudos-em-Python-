95a8e6fbde2dbcbe9792f118f1b051136d80d987
Hugo Antunes <86171824+HugoAntt@users.noreply.github.com>
2022-10-14 18:57:29 -0300

    Segundos passos

--- Ap/app.py
+++ Ap/app.py
@@ -1,5 +1,15 @@
 
-nome = 'Hugo'
-idade = 'isso aqui'
-print(nome)
-print(idade)
+class Computador:
+
+    def __init__(self, marca, modelo, ano, geracao):
+        self.marca = ''
+        self.modelo = ''
+        self.ano = ''
+        self.geracao = ''
+
+    def ligar(self):
+        print(
+            f'Ligando o {self.marca} modelo {self.modelo} ano {self.ano} geracao {self.geracao}')
+
+computador1 = Computador('Positivo', 'Xp', '2017', '5')
+computador1.ligar()
