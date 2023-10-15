054f6ccc6ce463012179946e01b623b7361da73d
HugoAntt <hugoantunesdasilvahugo@gmail.com>
2023-10-15 14:53:46 -0300

    Erro 'requests'

--- myscraper.py
+++ myscraper.py
@@ -1,4 +1,3 @@
-import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,8 +5,9 @@ url = "https://www.pichau.com.br/hardware/ssd"
 
 site = requests.get(url)
 
-if response.status_code == 200:
+soup = BeautifulSoup(site.content, "html.parser")
+placas = soup.find_all('div', class_="MuiCardContent-root jss131")
+ultimaPagina = soup.find('button', class_='text-page last')
+marca = placas[0].find('a', class_='list_product')
 
- soup = BeautifulSoup(site.content, "html.parser")
- placas = soup. find_all('div', class_="MuiCardContent-root jss131")
- ultimaPagina
+print(marca)
