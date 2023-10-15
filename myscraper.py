f5a1f33daa4506ef3c61cdd5d598e47536f81690
HugoAntt <hugoantunesdasilvahugo@gmail.com>
2023-10-15 12:52:45 -0300

    Readme

--- myscraper.py
+++ myscraper.py
@@ -1,1 +1,13 @@
-print("Olá, mundo!")
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.pichau.com.br/hardware/ssd"
+
+site = requests.get(url)
+
+if response.status_code == 200:
+
+ soup = BeautifulSoup(site.content, "html.parser")
+ placas = soup. find_all('div', class_="MuiCardContent-root jss131")
+ ultimaPagina
